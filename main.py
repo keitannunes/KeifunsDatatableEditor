ca3ee8e7eca200247187107181237a405bdcd309
keitannunes <keitannunes@gmail.com>
2024-09-23 13:53:12 -0400

    removed debugging code from main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from src import program
-from src import fumen
 
 if __name__ == "__main__":
-    fumen.add_ura_to_song('g0l02s', 'C:\\Users\\knunes\\Downloads\\Mood Swing.tja', 'C:\\Users\\knunes\\OneDrive - Jamieson Wellness\\Documents\\out')
-#
-# if __name__ == "__main__":
-#     ui = program.Program()
-#     ui.run()
+    ui = program.Program()
+    ui.run()
